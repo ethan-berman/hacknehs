16441b38e8109a18218b0703de5e9775a01d7763
ethan-berman <ethanberman@Ethans-MacBook-Pro.local>
2017-11-04 09:24:38 -0400

    Setup complete

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+app.get('/script.js', function(req, res){
+	res.sendFile(__dirname + '/script.js');
+});
+app.get('/style.css', function(req, res){
+	res.sendFile(__dirname +  '/style.css');
+});
+
+io.on('connection', function(socket){
+	console.log('a user connected');
+	
+	socket.on('connection', function(){
+
+	});
+});
+
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
