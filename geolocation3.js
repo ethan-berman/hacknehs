d4354d8be4129578ced0f77741120643ac4c1037
CZheng19 <czheng18@gmail.com>
2017-11-04 18:31:26 -0400

    Add files via upload

--- geolocation3.js
+++ geolocation3.js
@@ -1,5 +1,5 @@
 
-function calculateRouteFromAtoB (platform) {
+function calculateRouteFromAtoB (platform) {. //Tanay
   var router = platform.getRoutingService(),
     routeRequestParams = {
       mode: 'fastest;car',
@@ -13,12 +13,72 @@ function calculateRouteFromAtoB (platform) {
       waypoint1: '42.3601,-71.0589'  // Location B
     };
 
+var geocoder;
+  var map;
+  var address1;
+  var address2;
+  var lat1;
+  var lng1;
 
-  router.calculateRoute(
-    routeRequestParams,
-    onSuccess,
-    onError
-  );
+
+  function initialize() {
+    geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(-34.397, 150.644);
+    var mapOptions = {
+      zoom: 8,
+      center: latlng
+    }
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  }
+
+  function codeAddress(address) {
+    
+    // console.log(results[0].geometry.location);
+
+   
+    geocoder.geocode( { 'address': address}, function(results, status) {
+
+
+      console.log(results[0].geometry.location.lat())
+      console.log(results[0].geometry.location.lng())
+      lat1=results[0].geometry.location.lat()
+      lng1=results[0].geometry.location.lng()
+
+       waypoint0: results[0].geometry.location.lat().toString() +", "+results[0].geometry.location.lng().toString(),
+       waypoint1: results[0].geometry.location.lat().toString() + ", "  + results[0].geometry.location.lng().toString()
+
+      if (status == 'OK') {
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location
+        });
+
+        
+        
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+
+
+
+  }
+    
+
+
+  function compareAddress(){
+    var address = document.getElementById('address').value;
+    codeAddress(address)
+   var pethome = document.getElementById('pethome').value;
+    codeAddress(pethome)
+
+
+  }
+
+
+
+
 }
 /**
  * This function will be called once the Routing REST API provides a response
